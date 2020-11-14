4ed86477faed0355e403e03fbefcd83cd709aafd
SPIGS <paspielvogel@gmail.com>
2020-11-14 03:45:38 -0500

    Fix database 3.

--- WebApp.py
+++ WebApp.py
@@ -5,7 +5,6 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS userdata (email text PRIMARY KEY not null, password text not null, firstName text not null, lastName text not null, userId int not null)")
-cur.execute("INSERT INTO userdata (email, password, firstName, lastName, userId) VALUES ('test@email.com', '12345', 'Dio', 'Brando', 1)")
 cur.execute("SELECT * FROM userdata")
 message = str(cur.fetchone())
 conn.commit()
